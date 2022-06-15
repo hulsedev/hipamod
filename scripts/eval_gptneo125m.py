a6dd6c4bae8bf14afec5dec247c7add1ae042097
sachaStewart <sachalevy26@gmail.com>
2022-06-15 16:35:49 -0400

    remove constraints on input length

--- scripts/eval_gptneo125m.py
+++ scripts/eval_gptneo125m.py
@@ -4,6 +4,5 @@ results = evaluator.simple_evaluate(
     model="gpt2",
     model_args="pretrained=EleutherAI/gpt-neo-125M",
     tasks=["lambada", "hellaswag"],
-    limit=50,
 )
 print(evaluator.make_table(results))
