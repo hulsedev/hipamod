f4d90a57b84a4fb6477f9cf91a9ee01d3db0f0ae
sachaStewart <sachalevy26@gmail.com>
2022-06-15 16:27:52 -0400

    update eval batch

--- scripts/eval_gptneo125m.sh
+++ scripts/eval_gptneo125m.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=eval_gptneo125m
+#SBATCH --output=trace/output.txt
+#SBATCH --error=trace/error.txt
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=12         # Ask for 6 CPUs
+#SBATCH --gres=gpu:32gb:1              # Ask for 1 GPU
+#SBATCH --mem=64G                 # Ask for 10 GB of RAM
+#SBATCH --time=3:00:00            # The job will run for 10 minutes
+
 module load python/3.7
 module load cuda/10.2/cudnn/7.6
 module load cudatoolkit/10.2
