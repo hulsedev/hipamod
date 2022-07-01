04f72685b34696fe99ad31d7466bc95267db173f
sachaStewart <sachalevy26@gmail.com>
2022-07-01 19:33:25 +0200

    update of launch script

--- scripts/finetune_fralbert.sh
+++ scripts/finetune_fralbert.sh
@@ -8,6 +8,7 @@
 #SBATCH --mem=64G                 # Ask for 10 GB of RAM
 #SBATCH --time=3:00:00            # The job will run for 10 minutes
 
+rm -rf trace && mkdir trace
 module load python/3.7
 module load cuda/10.2/cudnn/7.6
 module load cudatoolkit/10.2
